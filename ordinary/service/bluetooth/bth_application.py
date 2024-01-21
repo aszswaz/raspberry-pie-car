9711a203df02e84731c69eb7676316616343e593
aszswaz <aszswaz@163.com>
2024-01-21 22:42:01 +0800

    通过蓝牙的 GATT 协议接收指令

---
+++ ordinary/service/bluetooth/bth_application.py
@@ -0,0 +1,31 @@
+from dbus import Bus
+from dbus.service import Object, ObjectPath, method
+
+from .bth_service import BthService
+from .constants import *
+
+
+class BthApplication(Object):
+    obj_path = ObjectPath(f"{BUS_NAMESPACE}/BthApplication")
+    obj_name = f"{BUS_NAME}.BthApplication"
+
+    bth_service: BthService
+
+    def __init__(self, bus: Bus) -> None:
+        super().__init__(bus, self.obj_path, self.obj_name)
+        self.bth_service = BthService(bus)
+        return None
+
+    @method(
+        dbus_interface=DBUS_OM_IFACE,
+        out_signature="a{oa{sa{sv}}}"
+    )
+    def GetManagedObjects(self):
+        return {
+            self.bth_service.obj_path: {
+                GATT_SERVICE_INTERFACE: self.bth_service.get_properties()
+            },
+            self.bth_service.chtic.obj_path: {
+                GATT_CHARACTERISTIC_INTERFACE: self.bth_service.chtic.get_properties()
+            }
+        }
---
+++ ordinary/service/bluetooth/bth_service.py
@@ -0,0 +1,39 @@
+from dbus.service import Object, ObjectPath, method
+import dbus
+
+from .constants import *
+from .bth_characteristic import BthCharacteristic
+
+
+class BthService(Object):
+    obj_path = ObjectPath(f"{BUS_NAMESPACE}/BthService")
+    obj_name = f"{BUS_NAME}.BthService"
+
+    # org.bluez.GattService 的属性
+    primary = True
+
+    def __init__(self, bus) -> None:
+        super().__init__(bus, self.obj_path, self.obj_name)
+        self.chtic = BthCharacteristic(bus, self.obj_path)
+        return None
+
+    @method(
+        dbus_interface=DBUS_PROPERTIES,
+        in_signature="s",
+        out_signature="a{sv}"
+    )
+    def GetAll(self, _) -> dict:
+        """
+        bluez 需要获取 BthService 的属性
+        """
+        return self.get_properties()
+
+    def get_properties(self):
+        return {
+            "UUID": SERVICE_UUID,
+            "Primary": self.primary,
+            "Characteristics": dbus.Array(
+                [self.chtic.obj_path],
+                signature="o"
+            )
+        }
