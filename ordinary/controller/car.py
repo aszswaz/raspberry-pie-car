2d74f58828a2b90b49b344861fae47f0ec1a5068
aszswaz <aszswaz@163.com>
2024-01-22 05:09:36 +0800

    用蓝牙控制小车已完成

--- ordinary/controller/car.py
+++ ordinary/controller/car.py
@@ -5,21 +5,21 @@ from .motor import Motor
 from .car_signal import CarSignal
 
 # 行驶速度，建议在 30 ~ 99 以内取值
-__speed = 50
+speed = 50
 # 小车的控制信号
-__signal: CarSignal
+signal: CarSignal
 # 小车的状态指示灯
 __led_green = 6
 __led_red = 5
 
 # 左前轮
-__left_front: Motor
+wheel_left_front: Motor
 # 右前轮
-__right_front: Motor
+wheel_right_front: Motor
 # 左后轮
-__left_back: Motor
+wheel_left_back: Motor
 # 右后轮
-__right_back: Motor
+wheel_right_back: Motor
 
 
 def init():
@@ -29,11 +29,11 @@ def init():
     GPIO.setup(__led_green, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(__led_red, GPIO.OUT, initial=GPIO.LOW)
 
-    global __left_front, __right_front, __left_back, __right_back
-    __left_front = Motor(0x06, 0x0A, 0x0E)
-    __right_front = Motor(0x1A, 0x12, 0x16)
-    __left_back = Motor(0x1E, 0x26, 0x22)
-    __right_back = Motor(0x32, 24, 25, True)
+    global wheel_left_front, wheel_right_front, wheel_left_back, wheel_right_back
+    wheel_left_front = Motor(0x06, 0x0A, 0x0E)
+    wheel_right_front = Motor(0x1A, 0x12, 0x16)
+    wheel_left_back = Motor(0x1E, 0x26, 0x22)
+    wheel_right_back = Motor(0x32, 24, 25, True)
 
     set_signal(CarSignal.STOPED)
 
@@ -42,8 +42,8 @@ def set_signal(value: CarSignal):
     """
     设置小车的控制信号
     """
-    global __signal
-    __signal = value
+    global signal
+    signal = value
 
     if value == CarSignal.READY:
         GPIO.output(__led_green, GPIO.HIGH)
@@ -55,61 +55,132 @@ def set_signal(value: CarSignal):
 
 
 def get_signal() -> CarSignal:
-    return __signal
+    return signal
+
+
+def stop():
+    wheel_left_front.stop()
+    wheel_right_front.stop()
+    wheel_left_back.stop()
+    wheel_right_back.stop()
+    pass
+
+
+def left_front():
+    """
+    向左前方平移
+    """
+    if signal == CarSignal.STOPED:
+        return
+    wheel_left_front.stop()
+    wheel_right_front.forward(speed)
+    wheel_left_back.forward(speed)
+    wheel_right_back.stop()
 
 
 def forward():
     """
     前进
     """
-    if __signal == CarSignal.STOPED:
+    if signal == CarSignal.STOPED:
         return
-    __left_front.forward(__speed)
-    __right_front.forward(__speed)
-    __left_back.forward(__speed)
-    __right_back.forward(__speed)
-    pass
+    wheel_left_front.forward(speed)
+    wheel_right_front.forward(speed)
+    wheel_left_back.forward(speed)
+    wheel_right_back.forward(speed)
+
+
+def right_front():
+    """
+    向右前方平移
+    """
+    if signal == CarSignal.STOPED:
+        return
+    wheel_left_front.forward(speed)
+    wheel_right_front.stop()
+    wheel_left_back.stop()
+    wheel_right_back.forward(speed)
+
+
+def rear_left():
+    """
+    向左后方平移
+    """
+    if signal == CarSignal.STOPED:
+        return
+    wheel_left_front.back(speed)
+    wheel_right_front.stop()
+    wheel_left_back.stop()
+    wheel_right_back.back(speed)
 
 
 def back():
     """
     后退
     """
-    if __signal == CarSignal.STOPED:
+    if signal == CarSignal.STOPED:
         return
-    __left_front.back(__speed)
-    __right_front.back(__speed)
-    __left_back.back(__speed)
-    __right_back.back(__speed)
+    wheel_left_front.back(speed)
+    wheel_right_front.back(speed)
+    wheel_left_back.back(speed)
+    wheel_right_back.back(speed)
+
+
+def rear_right():
+    """
+    向右后方平移
+    """
+    if signal == CarSignal.STOPED:
+        return
+    wheel_left_front.stop()
+    wheel_right_front.back(speed)
+    wheel_left_back.back(speed)
+    wheel_right_back.stop()
 
 
 def move_right():
     """
     向右平移
     """
-    if __signal == CarSignal.STOPED:
+    if signal == CarSignal.STOPED:
         return
-    __right_front.back(__speed)
-    __right_back.forward(__speed)
-    __left_front.forward(__speed)
-    __left_back.back(__speed)
+    wheel_right_front.back(speed)
+    wheel_right_back.forward(speed)
+    wheel_left_front.forward(speed)
+    wheel_left_back.back(speed)
 
 
 def move_left():
     """
     向左平移
     """
-    if __signal == CarSignal.STOPED:
+    if signal == CarSignal.STOPED:
         return
-    __right_front.forward(__speed)
-    __right_back.back(__speed)
-    __left_front.back(__speed)
-    __left_back.forward(__speed)
+    wheel_right_front.forward(speed)
+    wheel_right_back.back(speed)
+    wheel_left_front.back(speed)
+    wheel_left_back.forward(speed)
 
 
-def stop():
-    __left_front.stop()
-    __right_front.stop()
-    __left_back.stop()
-    __right_back.stop()
-    pass
+def trun_left():
+    """
+    左转
+    """
+    if signal == CarSignal.STOPED:
+        return
+    wheel_left_front.back(speed)
+    wheel_right_front.forward(speed)
+    wheel_left_back.back(speed)
+    wheel_right_back.forward(speed)
+
+
+def trun_right():
+    """
+    右转
+    """
+    if signal == CarSignal.STOPED:
+        return
+    wheel_left_front.forward(speed)
+    wheel_right_front.back(speed)
+    wheel_left_back.forward(speed)
+    wheel_right_back.back(speed)
--- ordinary/service/commands.py
+++ ordinary/service/commands.py
@@ -4,25 +4,25 @@ ACTIONS = {
     # 停止
     0x0: car.stop,
     # 向左前移动
-    0x1: None,
+    0x1: car.left_front,
     # 向正前方移动
     0x2: car.forward,
     # 向右前方移动
-    0x3: None,
+    0x3: car.right_front,
     # 向左平移
     0x4: car.move_left,
     # 向右平移
     0x5: car.move_right,
     # 向左后方移动
-    0x6: None,
+    0x6: car.rear_left,
     # 向后方移动
     0x7: car.back,
     # 向右后方移动
-    0x8: None,
+    0x8: car.rear_right,
     # 左转
-    0x9: None,
+    0x9: car.trun_left,
     # 右转
-    0xA: None
+    0xA: car.trun_right
 }
 
 
